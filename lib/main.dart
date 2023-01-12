09a7549d9a5fe4e4dc1620684a40f4deadf53281
iamhemantgauhai <hemant20@navgurukul.org>
2023-01-12 17:35:48 +0530

    Added: Few Changes

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:mm_tracker_app/screens/login/login_screen.dart';
 import 'package:mm_tracker_app/screens/recycler/recyclerview_controller.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
   final prefs = await SharedPreferences.getInstance();
@@ -12,7 +13,6 @@ void main() async{
 class MyApp extends StatelessWidget {
   final bool isLoggedIn;
   const MyApp({Key? key, required this.isLoggedIn}) : super(key: key);
-
 
   @override
   Widget build(BuildContext context) {
