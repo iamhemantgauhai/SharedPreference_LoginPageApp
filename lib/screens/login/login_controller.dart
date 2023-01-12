2cd4a60c416d6546f30454c031143f104f555501
iamhemantgauhai <hemant20@navgurukul.org>
2023-01-12 17:22:25 +0530

    Added Shared Preferences

--- lib/screens/login/login_controller.dart
+++ lib/screens/login/login_controller.dart
@@ -2,26 +2,20 @@ import 'package:flutter/material.dart';
 
 TextEditingController employeeID = TextEditingController();
 TextEditingController password = TextEditingController();
+
 var regex =
     RegExp(r'^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[!@#\$&*~]).{8,}$');
 var employeeIDValidate = (currentValue) {
   var nonNullValue = currentValue ?? '';
-  if (nonNullValue.isEmpty) {
-    // return ("Employee ID is empty");
-  }
-  if (nonNullValue.length != 6) {
-    // return ("Must contain 6 digit");
-  }
+  if (nonNullValue.isEmpty) {}
+  if (nonNullValue.length != 6) {}
   return null;
 };
+
 var passwordValidate = (passCurrentValue) {
   var passNonNullValue = passCurrentValue ?? "";
   if (passNonNullValue.isEmpty) {
-    // return ("Password is empty");
   } else if (passNonNullValue.length < 9) {
-    // return ("Must be 8 characters");
-  } else if (!regex.hasMatch(passNonNullValue)) {
-    // return ("Must contain Upper, Lower, Digit and Special Character");
-  }
+  } else if (!regex.hasMatch(passNonNullValue)) {}
   return null;
 };
